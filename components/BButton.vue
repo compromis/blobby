0315dbbc09cbd293f77aeb4f447892766a01befc
Guillem <guillem@disedit.com>
2021-10-27 12:59:41 +0200

    Add BButton and BFooter

---
+++ components/BButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <component :is="tag" :to="to" :href="href" :class="['button', `button-${variant}`, `button-${size}`]">
+    <slot />
+  </component>
+</template>
+
+<script>
+  export default {
+    props: {
+      to: {
+        type: String,
+        default: null
+      },
+      href: {
+        type: String,
+        default: null
+      },
+      variant: {
+        type: String,
+        default: 'primary'
+      },
+      size: {
+        type: String,
+        default: 'md'
+      }
+    },
+    computed: {
+      tag () {
+        if (this.to) {
+          return 'nuxt-link'
+        }
+
+        if (this.href) {
+          return 'a'
+        }
+
+        return 'button'
+      }
+    }
+  }
+</script>
---
+++ components/BFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="footer text-muted">
+    <a href="https://compromis.net" target="_blank" rel="noopener" class="footer-logo d-block link-muted-to-black mb-3">
+      <compromis-logo mono />
+      <span class="visually-hidden">Compromís</span>
+    </a>
+    <p class="footer-text">
+      <strong>{{ campaign }}</strong> és una campanya de <strong><a href="https://compromis.net" target="_blank" rel="noopener" class="link-muted-to-black">Compromís</a></strong>
+    </p>
+  </footer>
+</template>
+
+<script>
+  import CompromisLogo from './CompromisLogo.vue'
+
+  export default {
+    components: {
+      CompromisLogo
+    },
+
+    props: {
+      campaign: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
