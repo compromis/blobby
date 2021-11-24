bdddaebe2c1a00872ac6a13057824b2423c6d5a2
lecarles <35672759+lecarles@users.noreply.github.com>
2021-11-24 11:29:27 +0100

    Add dark mode

---
+++ docs/sections/DarkDocs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Dark Mode</h2>
+    <input type="checkbox">
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
