bc6b9db52002fba18494b9f79f8f3e1bfcfe86ad
Guillem <guillem@disedit.com>
2022-01-12 14:20:50 +0100

    Add BCheckbox

---
+++ components/inputs/BCheckbox.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="form-check">
+    <input
+      :id="'checkbox' + name"
+      v-model="checked"
+      class="form-check-input"
+      type="checkbox"
+      :value="value"
+      :checked="checked"
+    >
+    <label :for="'checkbox' + name" class="form-check-label">
+      <slot />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: null
+    },
+
+    modelValue: {
+      type: Boolean,
+      default: false
+    },
+
+    value: {
+      type: String,
+      default: 'true'
+    }
+  },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    checked: {
+      get () {
+        return this.modelValue
+      },
+
+      set (value) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  }
+}
+</script>
