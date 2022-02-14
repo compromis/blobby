ccca1f86c01df1db709705a05dcc23606c940dc1
Guillem <guillem@disedit.com>
2022-02-14 11:53:58 +0100

    Make nav compataible with vue 2

--- components/nav/BNav.vue
+++ components/nav/BNav.vue
@@ -1,54 +1,56 @@
 <template>
-  <nav :class="['nav', { 'container': !fullWidth, 'container-fluid': fullWidth, 'nav-scrolled': scrolled, 'nav-expanded': expanded }]">
-    <div class="nav-card bg-white rounded-xs shadow">
-      <div class="nav-bar d-flex">
-        <slot name="logo">
-          <div class="nav-logo">
-            <div v-if="$slots['logo-prepend']" class="nav-logo-prepend">
-              <slot name="logo-prepend"></slot>
+  <div>
+    <nav :class="['nav', { 'container': !fullWidth, 'container-fluid': fullWidth, 'nav-scrolled': scrolled, 'nav-expanded': expanded }]">
+      <div class="nav-card bg-white rounded-xs shadow">
+        <div class="nav-bar d-flex">
+          <slot name="logo">
+            <div class="nav-logo">
+              <div v-if="$slots['logo-prepend']" class="nav-logo-prepend">
+                <slot name="logo-prepend"></slot>
+              </div>
+              <a href="https://compromis.net" class="nav-logo-compromis">
+                <compromis-logo :collapse-at="logoCollapseAt || collapseAt" />
+                <span class="visually-hidden">
+                  Compromís
+                </span>
+              </a>
+              <div v-if="$slots['logo-append']" class="nav-logo-append">
+                <slot name="logo-append"></slot>
+              </div>
             </div>
-            <a href="https://compromis.net" class="nav-logo-compromis">
-              <compromis-logo :collapse-at="logoCollapseAt || collapseAt" />
-              <span class="visually-hidden">
-                Compromís
-              </span>
-            </a>
-            <div v-if="$slots['logo-append']" class="nav-logo-append">
-              <slot name="logo-append"></slot>
-            </div>
+          </slot>
+
+          <transition name="fade">
+            <ul v-if="!expanded" :aria-title="menuTitle" :class="['nav-menu', 'nav-menu-abridged', 'd-none', `d-${collapseAt}-flex`]">
+              <slot></slot>
+            </ul>
+          </transition>
+
+          <slot name="basic-nav"></slot>
+
+          <div class="nav-toggle">
+            <button id="nav-toggler" :aria-expanded="expanded ? 'true' : 'false'" aria-controls="nav-drawer" @click="toggleDrawer">
+              <hamburger-icon v-if="!expanded" />
+              <close-icon v-else />
+              <span class="visually-hidden">Xarxa de webs de Compromís</span>
+            </button>
           </div>
-        </slot>
-
-        <transition name="fade">
-          <ul v-if="!expanded" :aria-title="menuTitle" :class="['nav-menu', 'nav-menu-abridged', 'd-none', `d-${collapseAt}-flex`]">
-            <slot></slot>
-          </ul>
+        </div>
+        <transition name="slide">
+          <div v-if="expanded" id="nav-drawer" :aria-title="menuTitle" class="nav-drawer">
+            <ul v-if="$slots.default" class="nav-menu-mobile">
+              <slot></slot>
+            </ul>
+            <b-nav-network />
+          </div>
         </transition>
-
-        <slot name="basic-nav"></slot>
-
-        <div class="nav-toggle">
-          <button id="nav-toggler" :aria-expanded="expanded ? 'true' : 'false'" aria-controls="nav-drawer" @click="toggleDrawer">
-            <hamburger-icon v-if="!expanded" />
-            <close-icon v-else />
-            <span class="visually-hidden">Xarxa de webs de Compromís</span>
-          </button>
-        </div>
       </div>
-      <transition name="slide">
-        <div v-if="expanded" id="nav-drawer" :aria-title="menuTitle" class="nav-drawer">
-          <ul v-if="$slots.default" class="nav-menu-mobile">
-            <slot></slot>
-          </ul>
-          <b-nav-network />
-        </div>
-      </transition>
-    </div>
-  </nav>
-  <div class="nav-padding" />
-  <transition name="fade">
-    <div v-if="expanded" class="nav-backdrop" @click="expanded = false" />
-  </transition>
+    </nav>
+    <div class="nav-padding" />
+    <transition name="fade">
+      <div v-if="expanded" class="nav-backdrop" @click="expanded = false" />
+    </transition>
+  </div>
 </template>
 
 <script>
